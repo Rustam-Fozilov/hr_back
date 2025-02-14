69e71e7a6fa1b45c66c7836d2fc0e2ee30ff14d6
Rustam Fozilov <thisisrustam@gmail.com>
2025-02-14 17:24:56 +0500

    last change

--- routes/test.php
+++ routes/test.php
@@ -5,6 +5,5 @@ use Illuminate\Support\Facades\Route;
 use Maatwebsite\Excel\Facades\Excel;
 
 Route::any('test', function (Request $request) {
-    Excel::import(new \App\Imports\UsersImport(), 'users.xlsx');
     return success();
 });
